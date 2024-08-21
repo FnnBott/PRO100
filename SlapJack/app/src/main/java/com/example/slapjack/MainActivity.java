444d5294e4be60939207df7c63054be058378318
1cek4 <67841585+1cek4@users.noreply.github.com>
2024-08-21 16:21:29 -0600

    Update MainActivity.java

--- SlapJack/app/src/main/java/com/example/slapjack/MainActivity.java
+++ SlapJack/app/src/main/java/com/example/slapjack/MainActivity.java
@@ -34,9 +34,12 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            giveCards(v);
             return insets;
         });
         onInit();
+
+
     }
 
     private void onInit() {
@@ -61,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
         enemyName.setText(enemy.name);
 
         isPlayersTurn = true;
+
 
     }
 
